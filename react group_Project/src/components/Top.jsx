e30149a87abee6481feea861122008730a55bbb7
Chhairady <Chhairady08@gmail.com>
2026-04-06 14:41:25 +0700

    done

--- react group_Project/src/components/Top.jsx
+++ react group_Project/src/components/Top.jsx
@@ -2,10 +2,6 @@
 import Header from './Header';
 import Episode from './Episode'
 import './../componets_CSS/Top.css'
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 import logo from '../assets/logo.svg'
 import pink from '../assets/pink.jpg'
 import oggy from '../assets/oggy1.png'
@@ -14,71 +10,20 @@ import { FaXTwitter } from 'react-icons/fa6'
 import bob from '../assets/bob.avif'
 import spookiz from '../assets/spook.png'
 // import newVd from '../assets/new-vd.svg'
-=======
-=======
->>>>>>> 4433d95 (Added Introduction)
->>>>>>> bc3dad826c024344664cc796d8ccd835e4000e2d
 
-import pink from '../assets/pink.jpg'
-
-
-<<<<<<< HEAD
->>>>>>> b0c5912 (Intro)
-=======
-=======
-import logo from '../assets/logo.svg'
-import pink from '../assets/pink.jpg'
-import oggy from '../assets/oggy1.png'
-import { FaYoutube, FaTiktok, FaInstagram, FaFacebook } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
-import bob from '../assets/bob.avif'
-// import newVd from '../assets/new-vd.svg'
->>>>>>> b248b9c171a0e1c5b55a4b9786825572d72fbafb
->>>>>>> 4433d95 (Added Introduction)
 function Top() {
   return (
     <>
       <main>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-        {/* <div className='image'>
-          <img src="https://static-media.fox.com/fmc/prod/fts/artwork/VX-15115006/e5gbs24shff0agug.jpg" alt="" />
-        </div> */}
-        <div className='container'>
-          <div className='slide_wrapper'>
-            <div className='slider'>
-              <div className='slide'>
-                <div>
-                  <img src={pink} alt="" className='pink' />
-                </div>
-                <div className='sub_heading'>
-                  <div className='sub'>WELCOME TO KIDDIZ</div>
-                  <div className='heading'>Endless hilarious comedy, unexpected surprises & edge-of-your-seat action of the best in animation</div>
-                </div>
-              </div>
-            </div>
-=======
-=======
->>>>>>> 4433d95 (Added Introduction)
->>>>>>> bc3dad826c024344664cc796d8ccd835e4000e2d
-          <div className='image'>
-            <img src="https://static-media.fox.com/fmc/prod/fts/artwork/VX-15115006/e5gbs24shff0agug.jpg" alt="" />
-          </div>
-        <div className='container'>
+
+        <div className='container'/>
           <div>
             <img src={pink} alt="" className='pink'/>
           </div>
           <div className='sub_heading'>
             <div className='sub'>WELCOME TO KIDDIZ</div>
             <div className='heading'>Endless hilarious comedy, unexpected surprises & edge-of-your-seat action of the best in animation</div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b0c5912 (Intro)
-=======
-=======
+
         {/* <div className='image'>
           <img src="https://static-media.fox.com/fmc/prod/fts/artwork/VX-15115006/e5gbs24shff0agug.jpg" alt="" />
         </div> */}
@@ -95,8 +40,6 @@ function Top() {
                 </div>
               </div>
             </div>
->>>>>>> b248b9c171a0e1c5b55a4b9786825572d72fbafb
->>>>>>> 4433d95 (Added Introduction)
           </div>
         </div>
         <div className='mid'>
@@ -121,7 +64,6 @@ function Top() {
                 </div>
               </div>
             </div>
->>>>>>> bc3dad826c024344664cc796d8ccd835e4000e2d
           </div>
         </div>
         <section className='p-vd p-section is-scroll-active'>
@@ -169,73 +111,10 @@ function Top() {
           </div>
           <h5>@Sony Music Labels Inc.</h5>
         </div>
-
-        {/* <section className='p-vd p-section is-scroll-active'>
-          <div className='p-vd__inner'>
-            <div className='p-vd__title c-title'>
-              <h2 className='c-title__text'>
-                
-                  <img src={newVd} alt="" />
-                
-              </h2>
-            </div>
-          </div>
-        </section> */}
-        {/* <div className='p-vd__contents'>
-          <div className='p-vd__contents-main'>
-            <div className='p-vd__contents-main-icon-text'>
-              <img src={newText} alt="" />
-            </div>
-            <div className='p-vd__contents-main-icon-character'>
-              <img src={skull} alt="" />
-            </div>
-          </div>
-        </div> */}
-        {/* <div className='p-news p-section is-scroll-active' data-scroll-effect>
-          <div className='p-news__inner'>
-            <div className='p-news__header'>
-
-              <div className='p-news__header-title'>
-                <div className='p-news__header-title-item'></div>
-                <div className='p-news__header-nav'>
-                  <div className='p-news__header-nav-item --global ' >
-
-                  </div>
-                </div>
-
-              </div>
-            </div>
-            <div className='p-news__contents'>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
-          </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        </div> */}
-      </main >
-=======
-=======
->>>>>>> 4433d95 (Added Introduction)
-
         </div>
       </main>
       
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b0c5912 (Intro)
-=======
-=======
-        </div> */}
-      </main >
->>>>>>> b248b9c171a0e1c5b55a4b9786825572d72fbafb
->>>>>>> 4433d95 (Added Introduction)
->>>>>>> bc3dad826c024344664cc796d8ccd835e4000e2d
+
     </>
   );
 }
