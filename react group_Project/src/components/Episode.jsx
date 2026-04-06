08a64bdd2ab61d360406b58340dd72625aff4586
Sie Rormony <niin2015@gmail.com>
2026-04-06 15:12:40 +0700

    j nii

--- react group_Project/src/components/Episode.jsx
+++ react group_Project/src/components/Episode.jsx
@@ -1,67 +1,84 @@
-import React from 'react';
-import { useState } from 'react';
-import {movie} from "./episode_data";
-import "../componets_CSS/Episode.css"
-import { FaYoutube, FaTiktok, FaInstagram, FaFacebook } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
+import React from "react";
+import { useState } from "react";
+import { movie } from "./episode_data";
+import "../componets_CSS/Episode.css";
+import { FaYoutube, FaTiktok, FaInstagram, FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
-
- function Episode(){
+function Episode() {
   const movieName = Object.keys(movie);
   const [selectedMovie, setSelectedMovie] = useState(movieName[0]);
-  const [selectdSeason, setSeletedSeason] = useState(Object.keys(movie[movieName[0]])[0]);
-  const handleMovieClick = (name) =>{
-    setSelectedMovie(name);
-    setSeletedSeason(Object.keys(movie[name])[0]);
-  }
+  const getSeasons = (movieObj) =>
+  Object.keys(movieObj).filter((key) => key !== "video");
+const [selectdSeason, setSeletedSeason] = useState(
+  getSeasons(movie[movieName[0]])[0]
+);
+const handleMovieClick = (name) => {
+  setSelectedMovie(name);
+  setSeletedSeason(getSeasons(movie[name])[0]);
+};
 
-  return(
+  return (
     <>
       <div className="episode-page">
-      <h1 id ='movieLogo'>EPISODE</h1>
-      <div className="category-bar">
-        {movieName.map((name) => (
-          <button
-          key={name} onClick={()=> handleMovieClick(name)} className={selectedMovie === name ? "active" : ""}
-          >{name}
-          </button>
-       ))}
+        <h1 id="movieLogo">EPISODE</h1>
+        <div className="category-bar">
+          {movieName.map((name) => (
+            <button
+              key={name}
+              onClick={() => handleMovieClick(name)}
+              className={selectedMovie === name ? "active" : ""}
+            >
+              {name}
+            </button>
+          ))}
         </div>
         <div className="season-bar">
-          {Object.keys(movie[selectedMovie]).map((season) => (
-            <button key={season} onClick={() => setSeletedSeason(season)} className={selectdSeason === season ? "active" : ""}>{season}</button>
+          {getSeasons(movie[selectedMovie]).map((season) => (
+            <button
+              key={season}
+              onClick={() => setSeletedSeason(season)}
+              className={selectdSeason === season ? "active" : ""}
+            >
+              {season}
+            </button>
           ))}
         </div>
         <div className="episode-container">
-       <div className="episode-grid">
-        {movie[selectedMovie][selectdSeason].map((ep) => (
-          <div className="episode-card" key={ep.id}>
-            <img src={ep.img} alt={ep.title} />
-            <p>{ep.title}</p>
+          <div className="episode-grid">
+            {movie[selectedMovie][selectdSeason].map((ep) => (
+              <a
+                href={movie[selectedMovie].video}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={ep.id}
+              >
+                <div className="episode-card">
+                  <img src={ep.img} alt={ep.title} />
+                  <p>{ep.title}</p>
+                </div>
+              </a>
+            ))}
           </div>
-        ))}
-       </div>
-      </div>
-      </div>
-      <div id='btm'>
-          <h4>Website Terms of Use</h4>
-          <div id='footer'>
-            <FaYoutube />
-            <FaTiktok />
-            <FaInstagram />
-            <FaXTwitter />
-            <FaFacebook />
-            <span id='divider'>|</span>
-            <FaInstagram />
-            <FaXTwitter />
-            <span>for Cambodian</span>
-          </div>
-          <h5>@Sony Music Labels Inc.</h5>
         </div>
-</>
+      </div>
+      <div id="btm">
+        <h4>Website Terms of Use</h4>
+        <div id="footer">
+          <FaYoutube />
+          <FaTiktok />
+          <FaInstagram />
+          <FaXTwitter />
+          <FaFacebook />
+          <span id="divider">|</span>
+          <FaInstagram />
+          <FaXTwitter />
+          <span>for Cambodian</span>
+        </div>
+        <h5>@Sony Music Labels Inc.</h5>
+      </div>
+    </>
   );
-  
 }
-
 
 export default Episode;
